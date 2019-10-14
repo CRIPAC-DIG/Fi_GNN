e87752aedccb29a81bf41ac94a18c2f124a03f80
cyente <cuizeyu2016@ia.ac.cn>
2019-10-14 15:43:50 +0800

    Add files via upload

---
+++ util/model_saver.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import json
+import platform
+import numpy as np
+import os
+import collections
+
+"""
+save the result and the model of lightrnn
+includes itemdict
+         model
+         best auc
+"""
+
+
+def CTR_GNN_saver(session, config, best_auc, best_epoch):
+    saver = tf.train.Saver()
+    # best_auc_str = str(best_auc)
+    # best_auc_str = '_auc_' + best_auc_str.replace('.', '_') + '_epoch_' + str(best_epoch)
+    save_name = 'model_dim' + str(config.hidden_size) + '_' + str(config.gpu) + '.ckpt'
+    saver_path = saver.save(session, "save/model/" + save_name)
+    print("Model saved in file:", saver_path)
+
+
+def CTR_GNN_loader(session, config, loadwhich=0):
+    saver = tf.train.Saver()
+    save_name = 'model_dim' + str(config.hidden_size) + '_' + str(config.gpu) + '.ckpt'
+    saver.restore(session, "save/model/" + save_name)
+    print("Model loaded in file:", "save/model/" + save_name)
